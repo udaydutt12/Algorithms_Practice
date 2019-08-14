fb05b8991d2dbeec9870fb28bd0bd735266e6f50
udutt <uday.dutt@ansys.com>
2019-08-14 16:49:00 -0500

    Create a Second way to solve the Collatz Conjecture

--- Collatz_Conjecture.py
+++ Collatz_Conjecture.py
@@ -3,6 +3,7 @@ Start with a number n > 1. Find the number of steps it takes
 to reach one using the following process: If n is even, divide it by 2.
  If n is odd, multiply it by 3 and add 1.
 """
+import math
 def Collatz_naive_solution(n):
     count=0
     while n!=1:
@@ -12,3 +13,15 @@ def Collatz_naive_solution(n):
             n=3*n+1
         count+=1
     return count
+    
+#The second solution will need to be trained before it becomes effective
+hashmap={}
+def Collatz_second_solution(n):
+    count=0
+    while n!=1:
+        if n in hashmap:
+            return hashmap[n]
+        else:
+            hashmap[n]=Collatz_naive_solution(n)
+            return hashmap[n]
+    
