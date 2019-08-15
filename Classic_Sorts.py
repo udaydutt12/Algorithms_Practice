35fa3eecbdaa1cbf46e9b0382e8fcf7ba62b08f1
udutt <uday.dutt@ansys.com>
2019-08-15 12:25:10 -0500

    define all the sorts that I want to do today

--- Classic_Sorts.py
+++ Classic_Sorts.py
@@ -1,1 +1,33 @@
 #Do Classic Sorting Algorithms
+
+#Bubble Sort
+def bubble_sort(list1):
+    n=len(list1)
+    for i in range(n-1):
+        for j in range(i+`,n):
+            if list1[i]>list1[j]:
+                temp=list1[i]
+                list1[i]=list1[j]
+                list1[j]=temp
+    return list1
+    
+#Insertion Sort
+def insertion_sort(list1):
+    
+#quick sort
+def quick_sort(list1):
+
+#selection sort
+def selection_sort(list1):
+
+#merge sort 
+def merge_sort(list1):
+
+#Heap Sort
+def heap_sort(list1):
+
+#counting sort
+def counting_sort(list1):
+    
+    
+def radix_sort(list1):
